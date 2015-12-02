d660f0b943506bf2e0c04b65853b6db2640cbe3a
parasquid <tristan.gomez@gmail.com>
2015-12-03 01:15:48 +0800

    delegate the tracking of gifts to a repository

--- lib/one_up_app/user.rb
+++ lib/one_up_app/user.rb
@@ -2,23 +2,44 @@ require_dependency "one_up_app/gift"
 
 module OneUpApp
   class User
-    attr_reader :gifts_given, :gifts_received, :name
+    class UserRepository
+      attr_reader :gifts_given, :gifts_received
+      def initialize
+        @gifts_given = []
+        @gifts_received = []
+      end
+    end
 
-    def initialize(name:)
-      @gifts_given = []
-      @gifts_received = []
+    attr_reader :name
+
+    def initialize(name:, repository: UserRepository.new)
       @name = name
+      @repository = repository
     end
 
     def give(receiver, gift:)
       receiver.receive(gift)
       gift.given_by = self
-      @gifts_given << gift
+      repository.gifts_given << gift
     end
 
     def receive(gift)
       gift.received_by = self
-      @gifts_received << gift
+      repository.gifts_received << gift
+    end
+
+    def gifts_received
+      repository.gifts_received
+    end
+
+    def gifts_given
+      repository.gifts_given
+    end
+
+    private
+
+    def repository
+      @repository
     end
 
   end
