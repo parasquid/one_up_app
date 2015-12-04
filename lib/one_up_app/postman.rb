cf189143f1bad509166e146d0438b6527bc007c5
parasquid <tristan.gomez@gmail.com>
2015-12-05 00:45:36 +0800

    allow giving to multiple receivers

--- lib/one_up_app/postman.rb
+++ lib/one_up_app/postman.rb
@@ -5,10 +5,12 @@ module OneUpApp
     end
 
     def deliver(from:, to:)
-      from.give(package)
-      to.receive(package)
-      package.given_by = from
-      package.received_by = to
+      Array(to).each do |receiver|
+        from.give(package)
+        receiver.receive(package)
+        package.given_by = from
+        package.received_by = receiver
+      end
     end
 
     private
--- spec/one_up_app/postman_spec.rb
+++ spec/one_up_app/postman_spec.rb
@@ -1,29 +1,29 @@
 require "spec_helper"
 require "one_up_app/user"
+require "one_up_app/gift"
 require "one_up_app/postman"
 
 describe OneUpApp::Postman do
-  Given(:gift) { double("gift", to: nil) }
   context "sanity check" do
+    Given(:gift) { double("gift") }
     Given(:postman) { OneUpApp::Postman }
     When(:instance) { postman.new(gift) }
     Then { instance != nil }
   end
 
-  context "giving gifts" do
-    Given(:gift_options) {
-      {
-        description: "Lots of love",
-        image_url: "hug.png",
-        message: "Thanks for listening :)",
-        date_given: "2015-11-28"
-      }
+  Given(:gift_options) {
+    {
+      description: "Lots of love",
+      image_url: "hug.png",
+      message: "Thanks for listening :)",
+      date_given: "2015-11-28"
     }
-    Given(:gift) { OneUpApp::Gift.new(gift_options) }
-    Given(:giver) { OneUpApp::User.new(name: "giver") }
-    Given(:receiver) { OneUpApp::User.new(name: "receiver") }
-    Given(:postman) { OneUpApp::Postman.new(gift)}
-
+  }
+  Given(:gift) { OneUpApp::Gift.new(gift_options) }
+  Given(:giver) { OneUpApp::User.new(name: "giver") }
+  Given(:receiver) { OneUpApp::User.new(name: "receiver") }
+  Given(:postman) { OneUpApp::Postman.new(gift)}
+  context "giving a gift to a receiver" do
     When { postman.deliver(from: giver, to: receiver) }
 
     context "giving to a receiver records that same gift" do
@@ -46,5 +46,14 @@ describe OneUpApp::Postman do
         Then { gift.to_s == "giver gave receiver Lots of love on 2015-11-28.\nThanks for listening :)\n" }
       end
     end
+  end
+
+  context "giving a gift to multiple receivers" do
+    Given(:receiver_2) { OneUpApp::User.new(name: "receiver") }
+    Given(:receivers) { [receiver, receiver_2] }
+    When { postman.deliver(from: giver, to: receivers)}
+    Then { receiver.gifts_received.count == 1 }
+    Then { receiver_2.gifts_received.count == 1 }
+    Then { giver.gifts_given.count == 2 }
   end
 end
